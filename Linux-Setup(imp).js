18e3cd7e3275ee45fa76ad21eb4f07573ec6c988
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-05-18 00:31:03 +0530

    Linuks

--- Linux-Setup(imp).js
+++ Linux-Setup(imp).js
@@ -8,12 +8,26 @@ docker --version
 rm -rf webtop-config 
 rm -f docker-compose.yml
 
-// step-4
+// step-4 for Debian Xfce
 cat > docker-compose.yml <<EOF 
 services:
   debian-desktop:
     image: lscr.io/linuxserver/webtop:debian-xfce
     container_name: debian_gui
+    ports:
+      - "6080:3000"
+    volumes:
+      - ./webtop-config:/config
+    restart: unless-stopped
+EOF
+
+// step-4 for Ubuntu 
+
+@PS91104 ➜ /workspaces/lol (main) $ cat > docker-compose.yml <<EOF
+services:
+  ubuntu-desktop:
+    image: lscr.io/linuxserver/webtop:ubuntu-mate
+    container_name: ubuntu_gui
     ports:
       - "6080:3000"
     volumes:
